486493dac9cf61a1ab64b440f0a7b01342eb264c
sebastian <sebastian.villadam@autonoma.edu.co>
2019-11-10 21:10:14 -0500

    table logic integrated

--- js/AdmAnimalsController.js
+++ js/AdmAnimalsController.js
@@ -1,6 +1,6 @@
 'use strict'
 
-'use strict'
+var response;
 
 const Http = new XMLHttpRequest();
 const url='https://eyavqewaud.execute-api.ap-northeast-1.amazonaws.com/production/animal';
@@ -12,33 +12,35 @@ Http.onreadystatechange = (e) =>
 {
     if (Http.readyState == 4 && Http.status == 200) 
     {
-        var response = JSON.parse(Http.responseText);
+        response = JSON.parse(Http.responseText);
         
-        for (var key in response) 
-        {
-            //Bootstrap card structure for the animal info
-            var newText = "<div class='col-md-4'>" +  
-                            "<div class='card'>"+
-                                "<image src='" + response[key].animal_picture + "' class='card-img-top'></image>" +
-                                "<div class='card-body>" +
-                                    "<h5 class='card-title'>" + response[key].animal_name + "</h5><br>" +                        
-                                    "<p class='card-text'>Kind: " + response[key].animal_kind + "<br>" +
-                                    "Entry date: " + response[key].entry_date + "<br>" +
-                                    "Age: " + response[key].animal_age + "</p>" + 
-                                    "<form action='animalProfile.html'>" +
-                                        "<input type='hidden' name='id' value='" + response[key].animal_id + "'/>" +
-                                        "<input class='btn btn-primary' type='submit' value='find more'/>" +
-                                    "</form>" +                                    
-                                "</div>" +
-                            "</div>" +
-                        "</div>";
-
-            document.getElementById("animals_list").innerHTML += newText;  
-        }
+        printList();
     }
 }
 
-function limpiar()
+function printList()
 {
-    document.getElementById("animals_list").innerHTML = " ";
+    var strStatus = "";
+    for (var key in response) 
+    {
+        if(response[key].animal_status_id == 1)
+            strStatus = "Up for adoption";
+        if(response[key].animal_status_id == 2)
+            strStatus = "Euthanized";
+        if(response[key].animal_status_id == 3)
+            strStatus = "Adopted";
+
+        //Bootstrap table structure
+        var newText = "<tr>" +  
+                        "<th scope='row'>" + key + "</th>" +
+                        "<td>" + response[key].animal_name + "</td>" +
+                        "<td>" + strStatus + "</td>" +
+                        "<td>" + response[key].animal_id + "</td>" +  
+                        "<td><button class='btn btn-warning' data-toggle='modal' data-target='#statusModal'>Modify status</button></td>" +
+                        "<td><a href='modAnimal.html' class='btn btn-primary'>Modify data</a></td>" +
+                        "<td><button class='btn btn-danger' data-toggle='modal' data-target='#eliminateModal'>Eliminate</button></td>" +
+                    "</tr>";
+
+        document.getElementById("animals-table").innerHTML += newText;  
+    }
 }
