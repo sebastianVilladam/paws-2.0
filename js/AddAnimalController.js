25dce615ca9279db9802adffd5bd8f7267afa1eb
sebastian <sebastian.villadam@autonoma.edu.co>
2019-11-11 11:09:41 -0500

    add animal logic fixed

--- js/AddAnimalController.js
+++ js/AddAnimalController.js
@@ -2,62 +2,26 @@
 
 function submitInfo()
 {
-    //builds the params required for the submit as the URL and data
-    var url = "https://eyavqewaud.execute-api.ap-northeast-1.amazonaws.com/production/animal";
-    var str = "animal_name=" + document.getElementById("formInput").value + 
-              "&animal_kind=" + document.getElementById("formInput2").value +
-              "&animal_entry_date=" + document.getElementById("formInput3").value +
-              "&animal_created_on=" + document.getElementById("formInput4").value +
-              "&branch_id=" + document.getElementById("formInput5").value +
-              "&animal_status_id=" + document.getElementById("formInput6").value +
-              "&animal_age=" + document.getElementById("formInput7").value;
+    console.log("preparing....");
+    var animal_name = document.getElementById("formInput").value;
+    var animal_kind = document.getElementById("formInput2").value;
+    var animal_entry_date = document.getElementById("formInput3").value;
+    var animal_created_on = document.getElementById("formInput4").value;
+    var branch_id = document.getElementById("formInput5").value;
+    var animal_status_id = document.getElementById("formInput6").value;
+    var animal_age = document.getElementById("formInput7").value;
 
-    var ret = postSubmit(url, str) ;
-
-    if (ret.match(/^XHR error/)) 
-    {
-        console.log(ret);
-        return;
-    }
-    else
-        console.log(ret);
-}
-
-function postSubmit(url, str)
-{
-    var req;
-
-    if (window.XMLHttpRequest) 
-        req = new XMLHttpRequest();
-    else if (window.ActiveXObject) 
-        req = new ActiveXObject("Microsoft.XMLHTTP");
-        
-    if (req != undefined) 
-    {
-        req.overrideMimeType("application/json"); 
-        try 
-        {            
-            req.open("POST", url, false); 
-            alert("preparing");                
-            req.send(str); 
-            alert("sended");
-        }
-        catch(err) 
-        {
-            alert("couldnt complete request. Is JS enabled for that domain?\\n\\n" + err.message);
-            return false;
-        }
-
-        if (req.readyState == 4) 
-        {             
-            if (req.status == 200)  
-                //return req.responseText ; 
-                alert("good");
-            else
-                //return "XHR error: " + req.status +" "+req.statusText; 
-                alert("bad");
-        }
+    const data = {animal_name, animal_kind, animal_entry_date, animal_created_on, branch_id, animal_status_id, animal_age};
+    const options = {
+        method: 'POST',
+        header: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)        
     }
 
-    alert("req for getAsync is undefined");
+    fetch('https://eyavqewaud.execute-api.ap-northeast-1.amazonaws.com/production/animal', options).then(resp =>{
+        alert(resp);
+        console.log(resp);
+    });
 }
